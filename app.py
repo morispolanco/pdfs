b75e8ce83b1ff864b05a7572129d39bbc962e108
morispolanco <morispolanco@gmail.com>
2023-07-30 00:56:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+import os
+
+OPENAI_API_KEY=os.getenv(openai_api_key)
 
 def get_pdf_text(pdf_docs):
     text = ""
