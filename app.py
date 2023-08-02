cf86212d5f0a3492165dfbbe893426a8f5c0b828
morispolanco <morispolanco@gmail.com>
2023-08-02 00:14:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
+from tiktoken import tiktoken
 
 def get_pdf_text(pdf_docs):
     text = ""
