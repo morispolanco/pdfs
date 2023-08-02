3e88c1d2d8b9d7bea98af3835e1e0b6e5823867e
morispolanco <morispolanco@gmail.com>
2023-08-01 18:56:53 -0600

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from langchain.chains import ConversationalRetrievalChain
 from htmlTemplates import css, bot_template, user_template
 from langchain.llms import HuggingFaceHub
 import os
+import openai
 
 def get_pdf_text(pdf_docs):
     text = ""
